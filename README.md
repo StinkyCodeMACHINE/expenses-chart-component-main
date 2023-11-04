02e9f4c98350bc7d3efedf85b6d7ea15e90f39d6
PoopFace <danilsteam-88@hotmail.com>
2023-11-04 22:57:22 +0300

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Frontend Mentor - Expenses chart component solution
+
+This is a solution to the [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Live Site URL: [https://spectacular-unicorn-2078cc.netlify.app/](https://spectacular-unicorn-2078cc.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- JavaScript
+- Vue
+
+## Author
+
+- Frontend Mentor - [@StinkyCodeMACHINE](https://www.frontendmentor.io/profile/StinkyCodeMACHINE)
---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import json from "./json/data.json";
+
+import { ref } from "vue";
+const maxElem = ref(
+  json.lastSevenDays.reduce((max, cur) => {
+    return max.amount > cur.amount ? max : cur;
+  })
+);
+
+const graphFigHeight = 150;
+const diff = ((1 - json.totalPrevMonth / json.totalThisMonth) * 100).toFixed(1);
+</script>
+
+<template>
+  <h1 class="sr-only">Expenses chart</h1>
+  <main>
+    <div class="balance-info">
+      <div class="balance-info__label">My balance</div>
+      <div class="balance-info__amount">${{ json.balance }}</div>
+    </div>
+    <div class="spending-info">
+      <h2>Spending - Last 7 days</h2>
+      <div class="spending-info__chart-container">
+        <div v-for="elem in json.lastSevenDays">
+          <div
+            class="spending-info__chart-container__fig"
+            :style="{
+              height:
+                elem.amount === maxElem.amount
+                  ? `${graphFigHeight}px`
+                  : `${graphFigHeight * (elem.amount / maxElem.amount)}px`,
+              backgroundColor:
+                elem.amount === maxElem.amount
+                  ? `var(--cyan-hsl)`
+                  : `var(--soft-red)`,
+            }"
+          ></div>
+          <div class="spending-info__chart-container__tooltip">
+            ${{ elem.amount }}
+          </div>
+          <div class="spending-info__chart-container__label">
+            {{ elem.day }}
+          </div>
+        </div>
+      </div>
+      <div class="spending-info__stats-container">
+        <div>
+          <div class="spending-info__stats-container__total-label">
+            Total this month
+          </div>
+          <div class="spending-info__stats-container__total-amount">
+            ${{ json.totalThisMonth }}
+          </div>
+        </div>
+        <div>
+          <div class="spending-info__stats-container__diff-amount">
+            {{ diff > 0 ? `+${Math.abs(diff)}` : `-${Math.abs(diff)}` }}%
+          </div>
+          <div class="spending-info__stats-container__diff-label">
+            from last month
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
